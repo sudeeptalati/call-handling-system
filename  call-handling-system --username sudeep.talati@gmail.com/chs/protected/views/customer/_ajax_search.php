64f6ff4c72744fc24b4338034c7ff6ae4538c7da
sudeep.talati@gmail.com <sudeep.talati@gmail.com@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2012-03-02 22:38:31 +0000

    live search sorted

---  call-handling-system --username sudeep.talati@gmail.com/chs/protected/views/customer/_ajax_search.php
+++  call-handling-system --username sudeep.talati@gmail.com/chs/protected/views/customer/_ajax_search.php
@@ -8,12 +8,17 @@ $displayResults=$results->getData();
 <th>Customer Name</th>
 <th>Town</th>
 <th>Postcode</th>
+
+<th>Product</th>
 <th>Model Number</th>
 <th>Serial Number</th>
+<!-- 
 <th>Date of Purchase</th>
 <th>Warranty Date</th>
+
 <th>Warranty For Months</th>
 <th>Warranty untill</th>
+ -->
 </tr>
 
 
@@ -25,20 +30,24 @@ foreach ($displayResults as $row)
 	<td><?php echo CHtml::link($row->fullname, array('Servicecall/existingCustomer', 'customer_id'=>$row->id));?></td>
 	<td><?php echo $row->town;?></td>
 	<td><?php echo $row->postcode;?></td>
+	<td>
+		<?php echo $row->product->brand->name;?>
+		<?php echo $row->product->productType->name;?>	
+	</td>
 	<td><?php echo $row->product->model_number;?></td>
 	<td><?php echo $row->product->serial_number;?></td>
-	<td><?php echo date('d-M-y', $row->product->purchase_date);?></td>
-	<td><?php echo date('d-M-y', $row->product->warranty_date);?></td>
-	<td><?php echo $row->product->warranty_for_months;?></td>
+	<td><?php //echo date('d-M-y', $row->product->purchase_date);?></td>
+	<td><?php //echo date('d-M-y', $row->product->warranty_date);?></td>
+	<td><?php //echo $row->product->warranty_for_months;?></td>
 	<?php 
-	$warranty_date=$row->product->warranty_date;
-	$warranty_months=$row->product->warranty_for_months;
-	
-	$php_w_date=strtotime($warranty_date);
-	$warranty_until= strtotime(date("Y-M-d", $warranty_date) . " +".$warranty_months." month");
-	$res=date('d-M-Y', $warranty_until);
+//	$warranty_date=$row->product->warranty_date;
+//	$warranty_months=$row->product->warranty_for_months;
+//	
+//	$php_w_date=strtotime($warranty_date);
+//	$warranty_until= strtotime(date("Y-M-d", $warranty_date) . " +".$warranty_months." month");
+//	$res=date('d-M-Y', $warranty_until);
 	?>
-	<td><?php echo $res;?></td>
+	<td><?php //echo $res;?></td>
 	</tr>
 	
 <?php 	

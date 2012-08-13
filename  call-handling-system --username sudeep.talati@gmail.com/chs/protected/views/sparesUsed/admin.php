a45caf136b835b3c9d635442d13ae67666207d88
kruthika.bethur <kruthika.bethur@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2012-08-13 12:18:41 +0000

    Adding timestamp to file name is working.

---  call-handling-system --username sudeep.talati@gmail.com/chs/protected/views/sparesUsed/admin.php
+++  call-handling-system --username sudeep.talati@gmail.com/chs/protected/views/sparesUsed/admin.php
@@ -73,7 +73,7 @@ or <b>=</b>) at the beginning of each of your search values to specify how the c
 	
 //	$sparesModel = SparesUsed::model()->finalize();
 	
-//$sparesModel = SparesUsed::model()->uploadFile();
+//	$sparesModel = SparesUsed::model()->uploadFile();
 
 ?>
 

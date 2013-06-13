f6a7d18cef97c102ea223a31d9bcf3a45dec53ac
kruthika.bethur@gmail.com <kruthika.bethur@gmail.com@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2013-06-13 12:02:25 +0000

    Error in changing engineer resolved.

--- trunk/ call-handling-system --username sudeep.talati@gmail.com/chs/protected/views/servicecall/engg_job_report.php
+++ trunk/ call-handling-system --username sudeep.talati@gmail.com/chs/protected/views/servicecall/engg_job_report.php
@@ -45,57 +45,38 @@ $model = new Servicecall('search');
 				'value' => 'CHtml::link($data->service_reference_number, array("Servicecall/".$data->id))',
 		 		'type'=>'raw',
         ),
+		array(
+				'name'=>'job_status_id',
+				'value'=>'JobStatus::item("JobStatus",$data->job_status_id)',
+				'filter'=>JobStatus::items('JobStatus'),
+		),
+		array(
+				'name'=>'fault_date', 'value'=>'date("d-M-Y",$data->fault_date)'
+		),
+		'net_cost',
 		
-		array('header' => 'Customer',
-            	'name'=>'customer_name','value'=>'$data->customer->fullname'),
-		array('name'=>'customer_town','value'=>'$data->customer->town'),
-		array('name'=>'customer_postcode','value'=>'$data->customer->postcode'),
-		//'product_id',
-		array(	'header' => 'Product',
-            	'name'=>'product_name',
-				'value'=>'$data->product->brand->name." ".$data->product->productType->name',
-				'filter'=>false),
-		
-		
+		array('header' => 'Customer', 'value'=>'$data->customer->fullname'),
+		array('header' => 'Address', 'value'=>'$data->customer->postcode'),
+		array('header' => 'Brand', 'value'=>'$data->product->brand->name'),
+		array('header' => 'Product Type', 'value'=>'$data->product->productType->name'),
+		array('header' => 'Model Number', 'value'=>'$data->product->model_number'),
+		array('header' => 'Serial Number', 'value'=>'$data->product->serial_number'),
+		array('header' => 'Contract Type', 'value'=>'$data->contract->contractType->name'),
+		'insurer_reference_number',
+		'fault_description',
+		'work_carried_out',
 		array(
 			'name'=>'engineer_id',
 			'value'=>'Engineer::item("Engineer",$data->engineer_id)',
 			'filter'=>Engineer::items('Engineer'),
 		),
-		//'created_by_user_id',
-		array('header' => 'RaisedBy',
-            	'name'=>'user_name','value'=>'$data->createdByUser->name','filter'=>false),
-		
-		
-		array(
-			'name'=>'job_status_id',
-			'value'=>'JobStatus::item("JobStatus",$data->job_status_id)',
-			'filter'=>JobStatus::items('JobStatus'),
-		),
-		array(
-			'name'=>'fault_date', 'value'=>'date("d-M-Y",$data->fault_date)'
-		),
-		
-//		array(
-//			'class'=>'CButtonColumn',
-//			'template'=>'{view}',
-//		),
 	),
 )); ?>
 
 
 
 <?php 
-//	if($startDate == '')
-//	{
-//		echo "Start Date is empty";
-//		$startDate = "";
-//	}
-//	if($endDate == '')
-//	{
-//		echo "end Date is empty";
-//		$endDate = "";
-//	}
+
 	$url=Yii::app()->request->getBaseUrl().'/servicecall/enggJobReport/?engg_id='.$engg_id.'&status_id='.$status_id.'&startDate='.$startDate.'&endDate='.$endDate;
 	echo CHtml::link('Export to excel',$url);
 ?>

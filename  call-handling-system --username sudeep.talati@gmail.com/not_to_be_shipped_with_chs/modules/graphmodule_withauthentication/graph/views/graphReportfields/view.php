15edd4b8dd60db87b62e746c5954baf20f3dde36
sudeep.makku@gmail.com <sudeep.makku@gmail.com@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2014-10-07 17:25:48 +0000

    Graph Module With Servicecall Reports Finished

---  call-handling-system --username sudeep.talati@gmail.com/not_to_be_shipped_with_chs/modules/graphmodule_withauthentication/graph/views/graphReportfields/view.php
+++  call-handling-system --username sudeep.talati@gmail.com/not_to_be_shipped_with_chs/modules/graphmodule_withauthentication/graph/views/graphReportfields/view.php
@@ -1,29 +1,21 @@
+<?php include('graph_menu.php'); ?>   
+ 
+ 
 <?php
-/* @var $this GraphReportfieldsController */
-/* @var $model GraphReportfields */
-
-$this->breadcrumbs=array(
-	'Graph Reportfields'=>array('index'),
-	$model->id,
-);
-
 $this->menu=array(
-	array('label'=>'List GraphReportfields', 'url'=>array('index')),
+	array('label'=>'List GraphReportfields', 'url'=>array('admin')),
 	array('label'=>'Create GraphReportfields', 'url'=>array('create')),
-	array('label'=>'Update GraphReportfields', 'url'=>array('update', 'id'=>$model->id)),
-	array('label'=>'Delete GraphReportfields', 'url'=>'#', 'linkOptions'=>array('submit'=>array('delete','id'=>$model->id),'confirm'=>'Are you sure you want to delete this item?')),
-	array('label'=>'Manage GraphReportfields', 'url'=>array('admin')),
 );
 ?>
 
-<h1>View GraphReportfields #<?php echo $model->id; ?></h1>
+<h1> Report Fields #<?php echo $model->field_name; ?></h1>
 
 <?php $this->widget('zii.widgets.CDetailView', array(
 	'data'=>$model,
 	'attributes'=>array(
-		'id',
-		'report_type',
-		'field_name',
+		//'id',
+		//'report_type',
+		//'field_name',
 		'field_type',
 		'field_relation',
 		'field_label',

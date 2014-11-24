d5897bfd883a5ebdbbc21f4cc6fa10989c28ff18
sweetpullo@gmail.com <sweetpullo@gmail.com@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2014-11-24 14:30:33 +0000

    created method getdatafordesktop

---  call-handling-system --username sudeep.talati@gmail.com/not_to_be_shipped_with_chs/modules/gomobile/gomobileServer/gomobile/protected/controllers/ServerController.php
+++  call-handling-system --username sudeep.talati@gmail.com/not_to_be_shipped_with_chs/modules/gomobile/gomobileServer/gomobile/protected/controllers/ServerController.php
@@ -6,73 +6,65 @@ class ServerController extends Controller
 	{
 		
 		header('Access-Control-Allow-Origin: *');  
-		$d=$_POST['jsonData'];
-		//$d='{"Details":[{"id":"27422","servicecall_number":"125685","engineer_id":"90000000","servicecall":{"service_reference_number":"125685","fault_description":"installation fail","fault_date":"1410300000","product_id":"22207","job_finished_date":"","customer|town":"dewas"},"customer":{"name":" Purva"}},{"id":"27423","servicecall_number":"125686","engineer_id":"564","servicecall":{"service_reference_number":"125686","fault_description":"installation fail","fault_date":"1410300000","product_id":"22208","job_finished_date":"","customer|town":"dewas"},"customer":{"name":" Purva"}},{"id":"27424","servicecall_number":"125687","engineer_id":"435","servicecall":{"service_reference_number":"125687","fault_description":"installation failed","fault_date":"1410300000","product_id":"22208","job_finished_date":"","customer|town":"dewas"},"customer":{"name":" Purva"}},{"id":"27425","servicecall_number":"125688","engineer_id":"403","servicecall":{"service_reference_number":"125688","fault_description":"installation failed","fault_date":"1410300000","product_id":"22209","job_finished_date":"","customer|town":"SWINDON"},"customer":{"name":" Purva"}}]} ';
+		$datareceived=$_POST['jsonData'];
+		//$datareceived='{"Details":[{"id":"27422","servicecall_number":"125685","engineer_id":"90000000","servicecall":{"service_reference_number":"125685","fault_description":"installation fail","fault_date":"1410300000","product_id":"22207","job_finished_date":"","customer|town":"dewas"},"customer":{"name":" Purva"}},{"id":"27423","servicecall_number":"125686","engineer_id":"564","servicecall":{"service_reference_number":"125686","fault_description":"installation fail","fault_date":"1410300000","product_id":"22208","job_finished_date":"","customer|town":"dewas"},"customer":{"name":" Purva"}},{"id":"27424","servicecall_number":"125687","engineer_id":"435","servicecall":{"service_reference_number":"125687","fault_description":"installation failed","fault_date":"1410300000","product_id":"22208","job_finished_date":"","customer|town":"dewas"},"customer":{"name":" Purva"}},{"id":"27425","servicecall_number":"125688","engineer_id":"403","servicecall":{"service_reference_number":"125688","fault_description":"installation failed","fault_date":"1410300000","product_id":"22209","job_finished_date":"","customer|town":"SWINDON"},"customer":{"name":" Purva"}}]} ';
 		$servicecalls=array();
 		$sent_servicecalls=array();
 		$unsent_servicecalls=array();
 		
-		$r=(array)json_decode($d, true); 
+		$datareceived_array=(array)json_decode($datareceived, true); 
 		
-		
-		foreach($r as $value)
+		foreach($datareceived_array as $datareceived_array_value)
 		{
 			
-			foreach ($value as $p)
+			foreach ($datareceived_array_value as $p)
 			{
-			$service_reference_number=$p['service_reference_number'];	
-			$engineer_email=$p['engineer_email'];
-			if($this->ifengineeremailexists($engineer_email))
-			{
-				$model=new EngineerData;
-				$model->engineer_email=$engineer_email;
-				$x=array();
-				$x['service_reference_number']=$service_reference_number;
-				$x['engineer_email']=$engineer_email;
-				$x['customer_fullname']=$p['customer_fullname'];
-				$x['customer_postcode']=$p['customer_postcode'];
-				$x['data']=$p['servicecall'];
-				$model->data=json_encode($x);
-				$model->data_status_id='1';///since 1 is recvd from CHS
-				$ar=array();
-				
-				if($model->save())
+				$service_reference_number=$p['service_reference_number'];	
+				$engineer_email=$p['engineer_email'];
+				if($this->ifengineeremailexists($engineer_email))//calling function
 				{
-		
-				$ar['service_reference_number']=$service_reference_number;
-				$ar['message']='Servicecall Sent';
-				}
+					$model=new EngineerData;
+					$model->engineer_email=$engineer_email;
+					$x=array();
+					$x['service_reference_number']=$service_reference_number;
+					$x['engineer_email']=$engineer_email;
+					$x['customer_fullname']=$p['customer_fullname'];
+					$x['customer_postcode']=$p['customer_postcode'];
+					$x['data']=$p['servicecall'];
+					$model->data=json_encode($x);
+					$model->data_status_id='1';///since 1 is recvd from CHS
+					$ar=array();
+					if($model->save())
+						{
+						$ar['service_reference_number']=$service_reference_number;
+						$ar['message']='Servicecall Sent';
+						}///end of if $model->save
+					else
+						{
+						$ar['service_reference_number']=$service_reference_number;
+						$ar['message']='Servicecall cannot be saved on the server. Please try again.';
+						}///end of else $model->save
+						
+					array_push($sent_servicecalls,$ar);	
+				}///end of ifengineeremailexists
 				else
-				{
-				
-				$ar['service_reference_number']=$service_reference_number;
-				$ar['message']='Servicecall cannot be saved on the server. Please try again.';
-				}
-				array_push($sent_servicecalls,$ar);	
-				
-			}///end of foreach ($value as $p)
-			else
-			{	
-				$ar=array();
-				$ar['service_reference_number']=$service_reference_number;
-				$ar['message']='Servicecall Cannot be Sent as engineer email is not found on the server. Please contact us at www.rapportsoftware.co.uk';
+				{	
+					$ar=array();
+					$ar['service_reference_number']=$service_reference_number;
+					$ar['message']='Servicecall Cannot be Sent as engineer email is not found on the server. Please contact us at www.rapportsoftware.co.uk';
 
-				array_push($unsent_servicecalls,$ar);	
-			}
-		}///end iof foreach($r as $value)
-		
-		$servicecalls['unsent_servicecalls']=$unsent_servicecalls;
-		$servicecalls['sent_servicecalls']=$sent_servicecalls; 
-
-
-		
-		echo json_encode($servicecalls);
-	}
+					array_push($unsent_servicecalls,$ar);	
+				}///end of else ifengineeremailexists
+			}///end iof foreach($datareceived_array_value as $p)
 			
-			
-}///end of  actionIndex
+			$servicecalls['unsent_servicecalls']=$unsent_servicecalls;
+			$servicecalls['sent_servicecalls']=$sent_servicecalls; 
+			echo json_encode($servicecalls);
+		}///end of foreach ($datareceived_array as $datareceived_array_value)
+		
+	}///end of  actionIndex
 	
-	////////creating function getmyenggdetails
+	////////creating function Getengineerdataformobile
 	public function actionGetengineerdataformobile()
 	{
 		$engineer_email=$_GET['engineer_email'];
@@ -81,24 +73,18 @@ class ServerController extends Controller
 		if($this->verifyengineer($engineer_email,$engineer_pwd))
 		{
 		$engineer_model=EngineerData::model()->findAllByAttributes(array('engineer_email'=>$engineer_email,'data_status_id'=>'1'));
-		
 		$myarray=array();
-		
 		foreach ($engineer_model as $data)
 			{
-			//echo $data->data;
-			
 			array_push($myarray,json_decode($data->data));
 			$this->deleteengineerdatarecord($data->id);
-			}
-			
-			
+			}///end of foreach $engineer_model as $data
 			echo json_encode($myarray);
-			}////end of if
-			else
-			{
-				echo json_encode(array('message'=>'INVALID ENGINEER'));
-			}
+		}////end of if verifyengineer
+		else
+		{
+			echo json_encode(array('message'=>'INVALID ENGINEER'));
+		}///end of else verifyengineer
 	}/////end of actionGetengineerdataformobile
 	
 	public function actionGetdatafrommobile()
@@ -129,23 +115,29 @@ class ServerController extends Controller
 		else
 		{
 			echo json_encode(array('status'=>'FAILED','status_message'=>' The username or password is incorrect. Please try again.'));
-		}///end of else
+		}///end of else verifyengineer
 	}///end of actiongetdatafrommobile
 
+	
+	public function actionGetdatafordesktop()
+	{
+	$engineer_data_model=EngineerData::model()->findByAttributes(array('data_status_id'=>'3'));
+	echo $engineer_data_model->data;
+	}///end of actionGetdatafordesktop
+	
+	
 	
 	public function deleteengineerdatarecord($id)
 	{
 		$engineer_data_model=EngineerData::model()->findByPk($id);
 		if($engineer_data_model===null)
 			throw new CHttpException(404,'The requested page does not exist.');
-
-		$engineer_data_model->delete();
+			$engineer_data_model->delete();
 	
 	}////end of deleteengineerdatarecords
 	
 	public function verifyengineer($engineer_email,$engineer_pwd)
 	{
-	
 		if (Engineer::model()->findByAttributes(array('engineer_email'=>$engineer_email,'pwd'=>$engineer_pwd)))
 			return true;
 		else

6cde9820156a4d5b9d8b32c3bbc7eea1b75aa42c
sweetpullo@gmail.com <sweetpullo@gmail.com@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2014-12-10 18:13:56 +0000

    action account_id created

---  call-handling-system --username sudeep.talati@gmail.com/not_to_be_shipped_with_chs/modules/gomobile/gomobile/controllers/DefaultController.php
+++  call-handling-system --username sudeep.talati@gmail.com/not_to_be_shipped_with_chs/modules/gomobile/gomobile/controllers/DefaultController.php
@@ -23,7 +23,25 @@ class DefaultController extends RController
 		$this->render('Databyappointmentdate');
 	}///end of Databyappointmentdate
 	
-		
+	public function actionAccountsetup()
+	{
+	$this->render('accountsetup');
+	
+	}///end of accountsetup
+	
+	public function actionAccountsetupview()
+	{
+	
+	
+	$account_id=$_POST['account_id'];
+	$id=$_POST['advance_parameter_id'];
+	$advancesettingsmodel=AdvanceSettings::model()->findByPK(array('id'=>$id));
+	$advancesettingsmodel->value=$account_id;
+	if ($advancesettingsmodel->save())
+		$this->render('accountsetup_view', array('account_id'=>$account_id));
+	
+	}///end of accountsetupview
+	
 	public function filters()
 	{
 		return array(

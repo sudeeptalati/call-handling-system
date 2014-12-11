782cbf39eb2667276197231610084b729d724185
sweetpullo@gmail.com <sweetpullo@gmail.com@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2014-12-11 19:01:20 +0000

    New files added in servicecalls

---
+++  call-handling-system --username sudeep.talati@gmail.com/not_to_be_shipped_with_chs/modules/gomobile/gomobile/views/gmservicecalls/receiveservicecallfrommobile.php
@@ -0,0 +1,50 @@
+<?php include('gomobile_menu.php'); ?>  
+<button onclick="receive_data();">Recieve data from Mobile</button>
+
+<script>
+function receive_data() 
+{
+$.ajax({
+	///url:'http://127.0.0.1/purva/call_handling/not_to_be_shipped_with_chs/modules/gomobile/gomobileServer/gomobile/index.php?r=server/getdatafordesktop',
+  url:'http://www.rapportsoftware.co.uk/gomobileserver/gomobile/index.php?r=server/getdatafordesktop', 
+	type: 'get',	
+	data: {'engineer_email':'sweetpullo@gmail.com'}, 
+	success: function(data, status) {   
+				alert("Following data has been received from Mobile:"+data);
+				console.log(data);
+				setServicecallsStatus(data);
+				
+				
+      },
+      error: function(xhr, desc, err) {
+      console.log(xhr);
+	  alert("Details: " + desc + "\nError:" + err);
+      }
+	  
+	})
+}///end of function
+
+function setServicecallsStatus(data)
+{
+
+
+
+$.ajax({
+      url: 'index.php?r=gomobile/gmservicecalls/servicecallreceivedfromgomobileserver',
+      type: 'post',
+	  data: {'data':data},
+	  success: function(data, status) {   
+				alert("Success"+data);
+				window.location='<?php echo Yii::app()->request->baseUrl."/index.php?r=gomobile/gmservicecalls/receivedcalls" ?>';
+      },
+      error: function(xhr, desc, err) {
+        console.log(xhr);
+		alert("Details: " + desc + "\nError:" + err);
+      }
+    }); // end ajax call
+
+
+
+}
+
+</script>

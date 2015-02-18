4e63dc589e359d165be337af81a8b9d9f9a873ec
sudeep.makku@gmail.com <sudeep.makku@gmail.com@97661709-4bcf-08d6-7ffe-37b7ecc6ca4d>
2015-02-18 12:08:12 +0000

    Version changed Mysql Schema changed

--- trunk/ call-handling-system --username sudeep.talati@gmail.com/chs/protected/config/main.php
+++ trunk/ call-handling-system --username sudeep.talati@gmail.com/chs/protected/config/main.php
@@ -411,7 +411,7 @@ return array(
 		'company_address'=>'Demo',
 		'company_contact_details'=>'Telephone:00000000 Fax:00000000 E-mail:demo.co.uk',
 		'vat_in_percentage'=>'',	
-		'software_version'=>'11',	
+		'software_version'=>'12',	
 		'smtp_host'=>$smtp_host,
 		'smtp_username'=>$smtp_username,
 		'smtp_password'=>$smtp_password,
